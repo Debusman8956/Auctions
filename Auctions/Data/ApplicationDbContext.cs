0e156ff324a3a621f3a9f4631989b3384ace4f26
Debusman8956 <muhdmuk@gmail.com>
2024-11-25 10:44:54 +0100

    Add project files.

---
+++ Auctions/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Auctions.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auctions.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Listing> Listing { get; set; }
+
+        public DbSet<Bid> Bid { get; set; }
+
+        public DbSet<Comment> Comment { get; set; }
+    }
+}
---
+++ Auctions/Services/ListingsService.cs
@@ -0,0 +1,15 @@
+﻿using Auctions.Data;
+
+namespace Auctions.Service
+{
+    public class ListingsService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ListingsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
